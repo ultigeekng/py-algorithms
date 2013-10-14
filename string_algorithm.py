eb7c6d016c7c89ea6481ade8e32aac481d97c781
sgaralapti <sgarlapati@sushanth.com>
2013-10-14 16:53:36 +0530

    success

---
+++ string_algorithm.py
@@ -0,0 +1,33 @@
+def getIntegerComplement(n):
+  """This is simple logic for getting max substring in a string 
+     whose sum of first and second half should be equal and the
+     sub string should be of maximum length
+
+     condition: String should have integers only
+     """
+  if len(n)%2 == 0:
+    iter_num = len(n)
+  else:
+    iter_num = len(n) - 1
+  while iter_num > 0:
+    for i in range(len(n)):
+      if i + iter_num > len(n):
+        break
+      else:
+        temp1 = n[i: i + iter_num]
+        iter_mid = iter_num/2
+        first = temp1[0: iter_mid]
+        second = temp1[iter_mid: iter_num]
+        if returnList(first) == returnList(second):
+          return iter_num, temp1
+    iter_num -= 2
+  return 0
+
+def returnList(sub_sub_string):
+  """This wil return the sum of all the integers in the passed string"""
+  sum_string_integers = 0
+  for i in  [int(j) for j in str(sub_sub_string)]:
+    sum_string_integers += i
+  return sum_string_integers
+
+print getIntegerComplement("986561517416921217551395112859219257312")
