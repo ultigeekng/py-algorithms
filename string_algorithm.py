3feb40f6003eee6c2d6cf2e1c81dacef8ed3742b
vasu-garlapati <vasu2779@gmail.com>
2013-10-14 16:58:32 +0530

    Update string_algorithm.py

--- string_algorithm.py
+++ string_algorithm.py
@@ -18,12 +18,12 @@ def getIntegerComplement(n):
         iter_mid = iter_num/2
         first = temp1[0: iter_mid]
         second = temp1[iter_mid: iter_num]
-        if returnSumOfList(first) == returnSumOfList(second):
+        if returnSumOfStringIntegers(first) == returnSumOfStringIntegers(second):
           return iter_num, temp1
     iter_num -= 2
   return 0
 
-def returnSumOfList(sub_sub_string):
+def returnSumOfStringIntegers(sub_sub_string):
   """This wil return the sum of all the integers in the passed string"""
   sum_string_integers = 0
   for i in  [int(j) for j in str(sub_sub_string)]:
